525d8da6313cdb1261b279d65943f02483f4764a
Miguel Ángel Richter Flores <miguelrichter99@hotmail.com>
2021-10-16 12:58:34 -0500

    Segunda entrega

---
+++ src/Components/Notifications/NotifCmpt.jsx
@@ -0,0 +1,10 @@
+import "./Notifications.css"
+import React from "react";
+
+export default function NotifCmpt(props) {
+    return(
+        <div className="notif">
+            <p>{props.notifText} <input type="checkbox"></input></p>            
+        </div>
+    );
+}
--- src/Components/Notifications/Notifications.jsx
+++ src/Components/Notifications/Notifications.jsx
@@ -1,9 +1,29 @@
+import "./Notifications.css"
 import React from "react";
+import NotifCmpt from "./NotifCmpt";
 
 export default function Notifications() {
+    
+    const notifications = ["Notificación 1", "Notificación 2", "Notificación 3", "Notificación 4", "Notificación 5"];
+
     return(
-        <div>
-            <h1>Notifications Page</h1>
+        <div className="main-notif">
+            <div className="notif-box">
+                <div className="container-notif">
+                    <h1>Notificaciones</h1>
+                    <form>
+                        {notifications.map((notifEl) => {
+                            return(
+                                <NotifCmpt 
+                                    key = {notifications.indexOf(notifEl)}
+                                    notifText = {notifEl}
+                                />
+                            );
+                        })}
+                        <button type="submit">Notificar</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
