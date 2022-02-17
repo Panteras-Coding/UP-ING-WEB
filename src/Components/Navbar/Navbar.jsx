91caf79c9321eedefd9f1b529abe1cfe568353dc
Alan Raimond <alanraimondc@outlook.com>
2022-02-17 08:36:42 -0600

    Barra Responsiva

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,66 @@
-import React from "react";
+import React, { useState } from "react"
 import "./Navbar.css"
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return(
-        <div className="navContent">
-            <Link to="/">
-                <img alt={"inicioIcon"} src={require("../Assets/Inicio.svg").default} />
-            </Link>
-            <Link to="/messages">
-                <img alt={"chatIcon"} src={require("../Assets/Chat.svg").default} />
-            </Link>
-            <Link to="/notifications">
-                <img alt={"notificacionesIcon"} src={require("../Assets/Notificaciones.svg").default} />
-            </Link>
-            <Link to="/upload">
-                <img alt={"compartirIcon"} src={require("../Assets/Compartir.svg").default} />
-            </Link>
-            <Link to="/calendar">
-                <img alt={"calendarioIcon"} src={require("../Assets/Calendario_blanco.svg").default} />
-            </Link>
-            <Link to="/configuration">
-                <img alt={"engraneIcon"} src={require("../Assets/Engrane.svg").default} />
-            </Link>
-        </div>
+    const [navbarOpen, setNavbarOpen] = useState(false)
+
+    const handleToggle = () => {
+        setNavbarOpen(!navbarOpen)
+    }
+
+    return (
+        <>
+            <div className="navContent">
+                <Link to="/">
+                    <img alt={"inicioIcon"} src={require("../Assets/Inicio.svg").default} />
+                </Link>
+                <Link to="/messages">
+                    <img alt={"chatIcon"} src={require("../Assets/Chat.svg").default} />
+                </Link>
+                <Link to="/notifications">
+                    <img alt={"notificacionesIcon"} src={require("../Assets/Notificaciones.svg").default} />
+                </Link>
+                <Link to="/upload">
+                    <img alt={"compartirIcon"} src={require("../Assets/Compartir.svg").default} />
+                </Link>
+                <Link to="/calendar">
+                    <img alt={"calendarioIcon"} src={require("../Assets/Calendario_blanco.svg").default} />
+                </Link>
+                <Link to="/configuration">
+                    <img alt={"engraneIcon"} src={require("../Assets/Engrane.svg").default} />
+                </Link>
+            </div>
+            <nav className="navBarResponsive">
+                <button onClick={handleToggle}>{navbarOpen ? <img src={require("../Assets/close.svg").default} /> : <img src={require("../Assets/menu.svg").default} />}</button>
+
+                <div className={navbarOpen ? "open" : "close"}>
+                    <Link to="/">
+                        <img alt={"inicioIcon"} src={require("../Assets/Inicio.svg").default} />
+                        Home
+                    </Link>
+                    <Link to="/messages">
+                        <img alt={"chatIcon"} src={require("../Assets/Chat.svg").default} />
+                        Mensajes
+                    </Link>
+                    <Link to="/notifications">
+                        <img alt={"notificacionesIcon"} src={require("../Assets/Notificaciones.svg").default} />
+                        Notificationes
+                    </Link>
+                    <Link to="/upload">
+                        <img alt={"compartirIcon"} src={require("../Assets/Compartir.svg").default} />
+                        Upload
+                    </Link>
+                    <Link to="/calendar">
+                        <img alt={"calendarioIcon"} src={require("../Assets/Calendario_blanco.svg").default} />
+                        Calendario
+                    </Link>
+                    <Link to="/configuration">
+                        <img alt={"engraneIcon"} src={require("../Assets/Engrane.svg").default} />
+                        Ajustes
+                    </Link>
+                </div>
+            </nav>
+        </>
     );
 }
