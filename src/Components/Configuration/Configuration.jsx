42932c2f6be20f0397858777affc0ad6910b6f09
GabrielaMason <0202578@up.edu.mx>
2022-06-24 17:26:03 -0500

    Inicio Novedades

--- src/Components/Configuration/Configuration.jsx
+++ src/Components/Configuration/Configuration.jsx
@@ -7,13 +7,13 @@ export default function Configuration() {
             <div className="ajustes-box">
                 <div className="container-config">
                 <h1>Ajustes</h1>
-                    <div className="btn">                
+                    <div className="btnConfg">                
                         <p>&ensp;<i className="fas fa-key"></i>&emsp;Cambio de contraseña</p>
                     </div>
-                    <div className="btn"><p>&ensp;<i className="far fa-question-circle"></i>&emsp;Ayuda</p></div>
-                    <div className="btn"><p>&ensp;<i className="fas fa-sign-out-alt"></i>&emsp;Cerrar sesión</p></div>
-                    <div className="btn"><p>&ensp;<i className="fas fa-wrench"></i>&emsp;Creditos y desarrolladores</p></div>
-                    <div className="btn"><p>&ensp;<i className="fas fa-exclamation-circle"></i>&emsp;Aviso de privacidad y contacto</p></div>
+                    <div className="btnConfg"><p>&ensp;<i className="far fa-question-circle"></i>&emsp;Ayuda</p></div>
+                    <div className="btnConfg"><p>&ensp;<i className="fas fa-sign-out-alt"></i>&emsp;Cerrar sesión</p></div>
+                    <div className="btnConfg"><p>&ensp;<i className="fas fa-wrench"></i>&emsp;Creditos y desarrolladores</p></div>
+                    <div className="btnConfg"><p>&ensp;<i className="fas fa-exclamation-circle"></i>&emsp;Aviso de privacidad y contacto</p></div>
                 </div>
             </div>
         </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,115 +1,43 @@
-import React from "react";
-import "./Home.css"
+import Multiselect from "multiselect-react-dropdown";
+import React, {useState} from "react";
+
 export default function Home() {
-    return(
-        <div>
-<h1 class="novedades" >Novedades</h1>
-<div class="container">
-    <section className="cards-section">
-    <div class="row">
-        <div class="col-md-4 ">
-            <div class="carbon-example flex-wrapper">
-            <img src="https://pixsector.com/cache/517d8be6/av5c8336583e291842624.png"></img>
-                <div class="inner-wrapper">
-                  <p>  Descripción</p>
-                  <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="carbon-example flex-wrapper">
-            <img src="https://pixsector.com/cache/517d8be6/av5c8336583e291842624.png"></img>
-                <div class="inner-wrapper">
-                <p>  Descripción</p>
-                  <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="carbon-example flex-wrapper">
-            <img src="https://pixsector.com/cache/517d8be6/av5c8336583e291842624.png"></img>
-                <div class="inner-wrapper">
-                <p>  Descripción</p>
-                  <p c>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-                </div>
-            </div>
-        </div>
-        
-    </div> 
-    </section>
-    <section className="cards-section">
-    <div class="row">
-        <div class="col-md-4 ">
-            <div class="carbon-example flex-wrapper">
-            <img src="https://pixsector.com/cache/517d8be6/av5c8336583e291842624.png"></img>
-                <div class="inner-wrapper">
-                  <p>  Descripción</p>
-                  <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="carbon-example flex-wrapper">
-            <img src="https://pixsector.com/cache/517d8be6/av5c8336583e291842624.png"></img>
-                <div class="inner-wrapper">
-                <p>  Descripción</p>
-                  <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="carbon-example flex-wrapper">
-            <img src="https://pixsector.com/cache/517d8be6/av5c8336583e291842624.png"></img>
-                <div class="inner-wrapper">
-                <p>  Descripción</p>
-                  <p c>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-                </div>
-            </div>
-        </div>
-        
-        
-    </div> 
-    </section>
-    <section className="cards-section">
-    <div class="row">
-        <div class="col-md-4 ">
-            <div class="carbon-example flex-wrapper">
-            <img src="https://pixsector.com/cache/517d8be6/av5c8336583e291842624.png"></img>
-                <div class="inner-wrapper">
-                  <p>  Descripción</p>
-                  <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="carbon-example flex-wrapper">
-            <img src="https://pixsector.com/cache/517d8be6/av5c8336583e291842624.png"></img>
-                <div class="inner-wrapper">
-                <p>  Descripción</p>
-                  <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="carbon-example flex-wrapper">
-            <img src="https://pixsector.com/cache/517d8be6/av5c8336583e291842624.png"></img>
-                <div class="inner-wrapper">
-                <p>  Descripción</p>
-                  <p c>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
-                </div>
-            </div>
-        </div>
-        
-        
-    </div> 
-    </section>
-    
-</div>
+    const [tags, setTags] = useState(["Inscripciones", "Escuela Práctica", "Verano", "General"]);
 
+    return ( 
+    <div>
+        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Subir una nueva novedad
+        </button>
 
-
-
-
+        <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Subir novedad</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+                <div className="form-floating">
+                    <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: "100px"}}></textarea>
+                    <label for="floatingTextarea2">Descripción</label>
+                </div>
+                <div class="form-floating mt-3">
+                <Multiselect 
+                    isObject={false}
+                    onRemove={(value) => {console.log(value)}}
+                    onSelect={(value) => {console.log(value)}}
+                    options={tags}
+                    placeholder="Tags"
+                />
+                </div>
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn" style={{backgroundColor: "#7D233A", color:"#FFFFFF", margin:"auto"}}>Compartir</button>
+            </div>
+            </div>
         </div>
-    );
+        </div>
+    </div>
+    )
 }
